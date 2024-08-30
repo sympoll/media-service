c7be1a4e22cc52640a1c371b447763355f4877e4
Idan <104992892+Idan-sh@users.noreply.github.com>
2024-08-30 17:40:27 +0300

    initial commit

---
+++ src/test/java/com/mtapizza/sympoll/mediaservice/MediaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtapizza.sympoll.mediaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
