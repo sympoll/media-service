f9ea11a75fe66269cf6a222c882fa1e071073734
Idan <104992892+Idan-sh@users.noreply.github.com>
2024-09-11 10:39:58 +0300

    Removed transactional annotation, causes bugs

--- src/main/java/com/mtapizza/sympoll/mediaservice/repository/image/ImageRepository.java
+++ src/main/java/com/mtapizza/sympoll/mediaservice/repository/image/ImageRepository.java
@@ -2,9 +2,11 @@ package com.mtapizza.sympoll.mediaservice.repository.image;
 
 import com.mtapizza.sympoll.mediaservice.model.image.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface ImageRepository extends JpaRepository<Image, Long> {
     Optional<Image> findByName(String name);
 }
